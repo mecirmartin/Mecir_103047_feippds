0b00ff42e2654368e3384ddb86ed35009462be6e
mecirmartin <mecir.martin@gmail.com>
2023-02-23 18:22:17 +0100

    feat: implement Bakery algorithm

--- main.py
+++ main.py
@@ -8,40 +8,43 @@ __email__ = "xmecirm@stuba.sk, xvavro@stuba.sk"
 __license__ = "MIT"
 
 from fei.ppds import Thread
-from time import sleep
 
-current_number: int = 0
-next_number: int = 0
+DEFAULT_NUM_RUNS = 10
+NUM_THREADS = 10
+
+flag = [False] * (DEFAULT_NUM_RUNS)
+number = [0] * (DEFAULT_NUM_RUNS)
+value: int = 0  # global counter
 
 
-def process(tid: int, num_runs: int):
+def process(tid: int, num_runs: int, msg: str):
     """
     Simulates a process.
-    
+
     :param tid: thread id
     :param num_runs: number of executions of the critical section
+    :param msg: message passed to 
     """
-    global current_number, next_number
+    global value
+    flag[tid] = True
+    number[tid] = max(number) + 1
+    flag[tid] = False
 
-    for _ in range(num_runs):
-        # process wants to enter critical section
-        turn: int = current_number
-        current_number += 1
+    for j in range(num_runs):
+        while flag[j]:
+            print(f'process {tid} assigning number')
+            pass
+        while (number[j] < number[tid] or (number[j] == number[tid] and j < tid)) and number[j] != 0:
+            print(f'process {tid} waiting')
+            pass
 
-        # wait for other processes to finish their execution of critical section
-        while turn != next_number:
-            continue
-
-        # execute critical section
-        print(f"Process {tid} runs a complicated computation!")
-        sleep(1)
-
-        # exit critical section
-        next_number += 1
+    # critical section
+    value += 1
+    print(msg, f'counter value: {value}')
+    number[tid] = 0
 
 
 if __name__ == '__main__':
-    DEFAULT_NUM_RUNS = 10
-    NUM_THREADS = 3
-    threads = [Thread(process, i, DEFAULT_NUM_RUNS) for i in range(NUM_THREADS)]
+    threads = [Thread(process, i, DEFAULT_NUM_RUNS,
+                      f"thread number: {i + 1}") for i in range(NUM_THREADS)]
     [t.join() for t in threads]
