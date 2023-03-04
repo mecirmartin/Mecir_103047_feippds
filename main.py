fdc04094c0a6bc22e914c2ea9a00ad82810df61a
mecirmartin <mecir.martin@gmail.com>
2023-03-04 20:53:34 +0100

    docs: update comments

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Program represents different sequences of using mutex
+This program represents my solution of Barber shop problem (second assignment for PPDS course at FEI STU). 
 
 University: STU Slovak Technical University in Bratislava
 Faculty: FEI Faculty of Electrical Engineering and Information Technology
@@ -23,7 +23,7 @@ class Shared(object):
 
     def __init__(self):
 
-        # TODO : Initialize patterns we need and variables
+        # Initialize patterns and variables we need
         self.mutex = Mutex()
         self.waiting_room = 0
         self.waiting_room_multiplex = Semaphore(SIZE_OF_WAITING_ROOM)
@@ -34,40 +34,40 @@ class Shared(object):
 
 
 def get_haircut(i):
-    # TODO: Simulate time and print info when customer gets haircut
+    # Simulate time and print info when customer gets haircut
     print(f'CUSTOMER: {i} gets haircut')
     sleep(1)
     pass
 
 
 def cut_hair():
-    # TODO: Simulate time and print info when barber cuts customer's hair
+    # Simulate time and print info when barber cuts customer's hair
     print('BARBER: cuts hair')
     sleep(1)
     pass
 
 
 def balk(i):
-    # TODO: Represents situation when waiting room is full and print info
+    # Represents situation when waiting room is full and print info
     print(f'CUSTOMER: {i} waiting room is full')
     sleep(0.5)
     pass
 
 
 def growing_hair(i):
-    # TODO: Represents situation when customer wait after getting haircut. So hair is growing and customer is sleeping for some time
+    # Represents situation when customer wait after getting haircut. So hair is growing and customer is sleeping for some time
     print(f"CUSTOMER: {i} hair is growing")
     sleep(1)
     pass
 
 
 def customer(i, shared):
-    # TODO: Function represents customers behavior. Customer come to waiting if room is full sleep.
-    # TODO: Wake up barber and waits for invitation from barber. Then gets new haircut.
-    # TODO: After it both wait to complete their work. At the end waits to hair grow again
+    # Function represents customers behavior. Customer come to waiting if room is full sleep.
+    # Wake up barber and waits for invitation from barber. Then gets new haircut.
+    # After it both wait to complete their work. At the end waits to hair grow again
 
     while True:
-        # TODO: Access to waiting room. Could customer enter or must wait? Be careful about counter integrity :)
+        # Access to waiting room. Could customer enter or must wait? Be careful about counter integrity :)
         shared.mutex.lock()
         if (shared.waiting_room == SIZE_OF_WAITING_ROOM):
             shared.mutex.unlock()
@@ -80,19 +80,16 @@ def customer(i, shared):
         shared.waiting_room += 1
         shared.mutex.unlock()
 
-        # if(temp <= SIZE_OF_WAITING_ROOM):
-        #     shared.waiting_room_multiplex.signal()
-
         print(f'CUSTOMER: {i} is in the room')
-        # TODO: Rendezvous 1
+        # Rendezvous 1
         shared.customer.wait()
         shared.barber.signal()
         get_haircut(i)
-        # TODO: Rendezvous 2
+        # Rendezvous 2
         shared.customer_done.wait()
         shared.barber_done.signal()
 
-        # TODO: Leave waiting room. Integrity again
+        # Leave waiting room. Integrity again
         shared.mutex.lock()
         shared.waiting_room -= 1
         shared.mutex.unlock()
@@ -101,16 +98,16 @@ def customer(i, shared):
 
 
 def barber(shared):
-    # TODO: Function barber represents barber. Barber is sleeping.
-    # TODO: When customer come to get new hair wakes up barber.
-    # TODO: Barber cuts customer hair and both wait to complete their work.
+    # Function barber represents barber. Barber is sleeping.
+    # When customer come to get new hair wakes up barber.
+    # Barber cuts customer hair and both wait to complete their work.
 
     while True:
-        # TODO: Rendezvous 1
+        # Rendezvous 1
         shared.customer.signal()
         shared.barber.wait()
         cut_hair()
-        # TODO: Rendezvous 2
+        # Rendezvous 2
         shared.customer_done.signal()
         shared.barber_done.wait()
 
