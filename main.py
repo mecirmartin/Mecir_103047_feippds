4007002faabd5d0728d9673108dff4e43be02434
mecirmartin <mecir.martin@gmail.com>
2023-03-11 07:49:27 +0100

    feat: initial commit

--- main.py
+++ main.py
@@ -1,149 +1,78 @@
-"""
-This program represents my solution of Barber shop problem (second assignment for PPDS course at FEI STU). 
+"""This module implements dinning philosophers problem.
 
-University: STU Slovak Technical University in Bratislava
-Faculty: FEI Faculty of Electrical Engineering and Information Technology
-Year: 2023
-"""
+ No solution is implemented.
+ """
 
-
-__authors__ = "Martin Mečír, Marián Šebeňa"
-__email__ = "mecir.martin@gmail.com, mariansebena@stuba.sk, xvavro@stuba.sk"
+__author__ = "Martin Mečír, Tomáš Vavro"
+__email__ = "mecir.martin@gmail.com"
 __license__ = "MIT"
 
-
-from fei.ppds import Mutex, Thread, Semaphore, print
+from fei.ppds import Thread, Mutex, Semaphore, print
 from time import sleep
 
-NUM_OF_CUSTOMERS = 5
-SIZE_OF_WAITING_ROOM = 3
+NUM_PHILOSOPHERS: int = 5
+NUM_RUNS: int = 10  # number of repetitions of think-eat cycle of philosophers
+RIGHT_HANDED_PHILOSOPHERS_IDS = [0,1,2,3]
+LEFT_HANDED_PHILOSOPHERS_IDS = [4]
 
 
-class Shared(object):
+class Shared:
+    """Represent shared data for all threads."""
 
     def __init__(self):
-        """
-            Init method of Shared class
-
-            :param self: reference to object
-        """
-        self.mutex = Mutex()
-        self.waiting_room = 0
-        self.waiting_room_multiplex = Semaphore(SIZE_OF_WAITING_ROOM)
-        self.customer = Semaphore(0)
-        self.barber = Semaphore(0)
-        self.customer_done = Semaphore(0)
-        self.barber_done = Semaphore(0)
+        """Initialize an instance of Shared."""
+        self.forks = [Mutex() for _ in range(NUM_PHILOSOPHERS)]
+        self.waiter: Semaphore = Semaphore(NUM_PHILOSOPHERS - 1)
 
 
-def get_haircut(i):
+def think(i: int):
+    """Simulate thinking.
+
+    Args:
+        i -- philosopher's id
     """
-    Simulate time and print info when customer gets haircut
+    print(f"Philosopher {i} is thinking!")
+    sleep(0.1)
 
-    :param i: thread id
+
+def eat(i: int):
+    """Simulate eating.
+
+    Args:
+        i -- philosopher's id
     """
-    print(f'CUSTOMER: {i} gets haircut')
-    sleep(1)
-    pass
+    print(f"Philosopher {i} is eating!")
+    sleep(0.1)
 
 
-def cut_hair():
+def philosopher(i: int, shared: Shared):
+    """Run philosopher's code.
+
+    Args:
+        i -- philosopher's id
+        shared -- shared data
     """
-    Simulate time and print info when barber cuts customer's hair
-    """
-    print('BARBER: cuts hair')
-    sleep(1)
-    pass
-
-
-def balk(i):
-    """
-    Represents situation when waiting room is full and print info
-
-    :param i: thread id
-    """
-    print(f'CUSTOMER: {i} waiting room is full')
-    sleep(0.5)
-    pass
-
-
-def growing_hair(i):
-    """
-    Represents situation when customer wait after getting haircut. So hair is growing and customer is sleeping for some time
-
-    :param i: thread id
-    """
-    print(f"CUSTOMER: {i} hair is growing")
-    sleep(1)
-    pass
-
-
-def customer(i, shared):
-    """
-    Function represents customers behavior. Customer come to waiting if room is full sleep.
-    Wake up barber and waits for invitation from barber. Then gets new haircut.
-    After it both wait to complete their work. At the end waits to hair grow again
-
-    :param i: thread id
-    :param shared: shared object with synchronization ADT
-    """
-    while True:
-        shared.mutex.lock()
-        if (shared.waiting_room == SIZE_OF_WAITING_ROOM):
-            shared.mutex.unlock()
-            balk(i)
-            continue
-        shared.mutex.unlock()
-
-        shared.waiting_room_multiplex.wait()
-        shared.mutex.lock()
-        shared.waiting_room += 1
-        shared.mutex.unlock()
-
-        print(f'CUSTOMER: {i} is in the room')
-        # Rendezvous 1
-        shared.customer.wait()
-        shared.barber.signal()
-        get_haircut(i)
-        # Rendezvous 2
-        shared.customer_done.wait()
-        shared.barber_done.signal()
-        shared.mutex.lock()
-        shared.waiting_room -= 1
-        shared.mutex.unlock()
-        shared.waiting_room_multiplex.signal()
-        growing_hair(i)
-
-
-def barber(shared):
-    """
-    Function barber represents barber. Barber is sleeping.
-    When customer come to get new hair wakes up barber.
-    Barber cuts customer hair and both wait to complete their work.
-
-    :param shared: shared object with synchronization ADT
-    """
-    while True:
-        # Rendezvous 1
-        shared.customer.signal()
-        shared.barber.wait()
-        cut_hair()
-        # Rendezvous 2
-        shared.customer_done.signal()
-        shared.barber_done.wait()
+    for _ in range(NUM_RUNS):
+        think(i)
+        # get forks
+        # shared.waiter.wait()
+        shared.forks[i].lock()
+        sleep(0.5)
+        shared.forks[(i+1) % NUM_PHILOSOPHERS].lock()
+        eat(i)
+        shared.forks[i].unlock()
+        shared.forks[(i + 1) % NUM_PHILOSOPHERS].unlock()
+        # shared.waiter.signal()
 
 
 def main():
-
-    shared = Shared()
-    customers = []
-
-    for i in range(NUM_OF_CUSTOMERS):
-        customers.append(Thread(customer, i, shared))
-    hair_stylist = Thread(barber, shared)
-
-    for t in customers + [hair_stylist]:
-        t.join()
+    """Run main."""
+    shared: Shared = Shared()
+    philosophers: list[Thread] = [
+        Thread(philosopher, i, shared) for i in range(NUM_PHILOSOPHERS)
+    ]
+    for p in philosophers:
+        p.join()
 
 
 if __name__ == "__main__":
