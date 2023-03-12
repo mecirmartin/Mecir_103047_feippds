212bbb282fc68c4ae706ec6c81f70b009bb94875
mecirmartin <mecir.martin@gmail.com>
2023-03-12 10:34:27 +0100

    feat: implement feasting savages problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-This program represents my solution of Barber shop problem (second assignment for PPDS course at FEI STU). 
+This program represents my solution of Feasting savages problem (fourth assignment for PPDS course at FEI STU). 
 
 University: STU Slovak Technical University in Bratislava
 Faculty: FEI Faculty of Electrical Engineering and Information Technology
@@ -7,16 +7,16 @@ Year: 2023
 """
 
 
-__authors__ = "Martin Mečír, Marián Šebeňa"
-__email__ = "mecir.martin@gmail.com, mariansebena@stuba.sk, xvavro@stuba.sk"
+__authors__ = "Martin Mečír"
+__email__ = "mecir.martin@gmail.com"
 __license__ = "MIT"
 
 
 from fei.ppds import Mutex, Thread, Semaphore, print
 from time import sleep
 
-NUM_OF_CUSTOMERS = 5
-SIZE_OF_WAITING_ROOM = 3
+NUM_OF_SAVAGES = 5
+SIZE_OF_COOKING_POT = 3
 
 
 class Shared(object):
@@ -27,122 +27,89 @@ class Shared(object):
 
             :param self: reference to object
         """
-        self.mutex = Mutex()
-        self.waiting_room = 0
-        self.waiting_room_multiplex = Semaphore(SIZE_OF_WAITING_ROOM)
-        self.customer = Semaphore(0)
-        self.barber = Semaphore(0)
-        self.customer_done = Semaphore(0)
-        self.barber_done = Semaphore(0)
+        self.pot_mutex = Mutex()
+        self.cooking_pot = SIZE_OF_COOKING_POT
+        self.empty_pot = Semaphore(0)
+        self.full_pot = Semaphore(0)
+
+        self.turnstile = Semaphore(0)
+        self.turnstile2 = Semaphore(1)
+        self.barrier_mutex = Mutex()
+        self.barrier_count = 0
 
 
-def get_haircut(i):
+def feast(i: int, shared: Shared):
     """
-    Simulate time and print info when customer gets haircut
+    Function which represents feasting of a savage.
 
-    :param i: thread id
-    """
-    print(f'CUSTOMER: {i} gets haircut')
-    sleep(1)
-    pass
-
-
-def cut_hair():
-    """
-    Simulate time and print info when barber cuts customer's hair
-    """
-    print('BARBER: cuts hair')
-    sleep(1)
-    pass
-
-
-def balk(i):
-    """
-    Represents situation when waiting room is full and print info
-
-    :param i: thread id
-    """
-    print(f'CUSTOMER: {i} waiting room is full')
-    sleep(0.5)
-    pass
-
-
-def growing_hair(i):
-    """
-    Represents situation when customer wait after getting haircut. So hair is growing and customer is sleeping for some time
-
-    :param i: thread id
-    """
-    print(f"CUSTOMER: {i} hair is growing")
-    sleep(1)
-    pass
-
-
-def customer(i, shared):
-    """
-    Function represents customers behavior. Customer come to waiting if room is full sleep.
-    Wake up barber and waits for invitation from barber. Then gets new haircut.
-    After it both wait to complete their work. At the end waits to hair grow again
-
-    :param i: thread id
+    :param i: id of a savage
     :param shared: shared object with synchronization ADT
     """
     while True:
-        shared.mutex.lock()
-        if (shared.waiting_room == SIZE_OF_WAITING_ROOM):
-            shared.mutex.unlock()
-            balk(i)
-            continue
-        shared.mutex.unlock()
+        shared.barrier_mutex.lock()
+        shared.barrier_count += 1
+        if shared.barrier_count == NUM_OF_SAVAGES:
+            print(f'SAVAGE: {i} arrived, barrier unlocking')
+            shared.turnstile2.wait()
+            shared.turnstile.signal()
+        shared.barrier_mutex.unlock()
+        print(f'SAVAGE: {i} waiting on barrier')
+        shared.turnstile.wait()
+        shared.turnstile.signal()
 
-        shared.waiting_room_multiplex.wait()
-        shared.mutex.lock()
-        shared.waiting_room += 1
-        shared.mutex.unlock()
+        shared.pot_mutex.lock()
+        # critical section
+        if shared.cooking_pot == 0:
+            print(f'SAVAGE: {i} went to take a portion, but pot is empty, waiting for refill')
+            shared.empty_pot.signal()
+            shared.full_pot.wait()
+        shared.cooking_pot -= 1
+        print(f'SAVAGE: {i} took a portion')
+        shared.pot_mutex.unlock()
 
-        print(f'CUSTOMER: {i} is in the room')
-        # Rendezvous 1
-        shared.customer.wait()
-        shared.barber.signal()
-        get_haircut(i)
-        # Rendezvous 2
-        shared.customer_done.wait()
-        shared.barber_done.signal()
-        shared.mutex.lock()
-        shared.waiting_room -= 1
-        shared.mutex.unlock()
-        shared.waiting_room_multiplex.signal()
-        growing_hair(i)
+        shared.barrier_mutex.lock()
+        shared.barrier_count -= 1
+        if shared.barrier_count == 0:
+            shared.turnstile.wait()
+            shared.turnstile2.signal()
+        shared.barrier_mutex.unlock()
+        shared.turnstile2.wait()
+        shared.turnstile2.signal()
 
 
-def barber(shared):
+def cook_food(shared: Shared):
     """
-    Function barber represents barber. Barber is sleeping.
-    When customer come to get new hair wakes up barber.
-    Barber cuts customer hair and both wait to complete their work.
+    Simulate time and print info when chef cooks the food
+
+    :param shared: shared object with synchronization ADT
+    """
+    print('CHEF: food is cooking')
+    sleep(1)
+    shared.cooking_pot = SIZE_OF_COOKING_POT
+
+
+def cook(shared: Shared):
+    """
+    Function cook represents chef. Chef is sleeping.
+    When savage signalizes that pot is empty, chef wakes up and cooks the food.
 
     :param shared: shared object with synchronization ADT
     """
     while True:
-        # Rendezvous 1
-        shared.customer.signal()
-        shared.barber.wait()
-        cut_hair()
-        # Rendezvous 2
-        shared.customer_done.signal()
-        shared.barber_done.wait()
+        shared.empty_pot.wait()
+        cook_food(shared)
+        shared.full_pot.signal()
 
 
 def main():
-
     shared = Shared()
-    customers = []
+    savages = []
 
-    for i in range(NUM_OF_CUSTOMERS):
-        customers.append(Thread(customer, i, shared))
-    hair_stylist = Thread(barber, shared)
+    for i in range(NUM_OF_SAVAGES):
+        savages.append(Thread(feast, i, shared))
+    chef = Thread(cook, shared)
 
-    for t in customers + [hair_stylist]:
+    for t in savages + [chef]:
         t.join()
 
 
